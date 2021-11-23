68d59499681af541051e5119943b6e9db64e8855
Karlcxu <contactkarlchoi@gmail.com>
2021-11-22 22:10:35 -0500

    Graphs work now

--- inc/html-top-index.php
+++ inc/html-top-index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,5 +9,7 @@
     <link rel="stylesheet" href="css/home_style.css">
     <link rel="stylesheet" href="css/navigation.css">
     <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/d3-selection/1.2.0/d3-selection.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/britecharts@2.10.0/dist/umd/bar.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/britecharts/dist/css/britecharts.min.css">
 </head>
