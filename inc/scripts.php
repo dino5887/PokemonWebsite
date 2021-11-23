aafdd97415e9ee873e8ba4e5bc3aff7cb981ec85
Brenden Becker <dino5887@outlook.com>
2021-11-22 21:45:16 -0500

    JS file

--- inc/scripts.php
+++ inc/scripts.php
@@ -1,2 +1,4 @@
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>;
 <script src="js/menu-highlighter.js"></script>;
+<script src="https://cdnjs.cloudflare.com/ajax/libs/d3-selection/1.2.0/d3-selection.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/britecharts@2.10.0/dist/umd/bar.min.js" type="text/javascript"></script>
