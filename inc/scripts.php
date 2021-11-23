17466bbaed0926aaf698df81617e1f83a55db305
Karlcxu <contactkarlchoi@gmail.com>
2021-11-22 22:20:37 -0500

    Cleaned up graph code

--- inc/scripts.php
+++ inc/scripts.php
@@ -1,9 +1,6 @@
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>;
 <script src="js/menu-highlighter.js"></script>;
-<script src="https://cdnjs.cloudflare.com/ajax/libs/d3-selection/1.2.0/d3-selection.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/britecharts@2.10.0/dist/umd/bar.min.js"></script>
-/* The above Two are for making Graphs */
-<script src="sss/sss.js"></script>
+<script src="sss/sss.js"></script>;
 <script>
     $('.slider').sss();
 </script>
